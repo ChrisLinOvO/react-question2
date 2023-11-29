fc577587e0e34254ae596afbfd7010fb7d18d051
Chris Lin <chrislin@ezcon.com>
2023-11-30 01:05:18 +0800

    add test

--- src/__test__/hooks/useCalendar.test.js
+++ src/__test__/hooks/useCalendar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import useCalendar from '../../src/hooks/useCalendar';
+import useCalendar from '../../hooks/useCalendar';
 
 // 模擬當前日期
 jest.mock('react', () => ({
